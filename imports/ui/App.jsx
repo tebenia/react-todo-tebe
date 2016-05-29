19d0458a9eb638975659be6d6167c32799519376
tebenia <tebe.deye@gmail.com>
2016-05-29 21:20:17 +0700

    Forms and Events
    Adding Form functionality to the app

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,25 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
+import ReactDOM from 'react-dom';
  
 import Task from './Task.jsx';
  
 // App component - represents the whole app
 class App extends Component {
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
  
   renderTasks() {
     return this.props.tasks.map((task) => (
@@ -18,6 +32,9 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" ref="textInput" placeholder="Type to add task"/>
+          </form>
         </header>
  
         <ul>
@@ -34,6 +51,6 @@ App.propTypes = {
  
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, {sort:{createdAt:-1}}).fetch(),
   };
 }, App);
