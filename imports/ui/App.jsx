fa952a38bd1e994adf2f5921f158a3f9abdb25c2
tebenia <tebe.deye@gmail.com>
2016-05-30 16:22:11 +0700

    Adding User Accounts

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
 import ReactDOM from 'react-dom';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
  
 import Task from './Task.jsx';
  
@@ -24,6 +26,8 @@ class App extends Component {
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username, // username of logged in user
     });
 
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
@@ -55,9 +59,16 @@ class App extends Component {
             <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)}/>
             Hide Completed Tasks
           </label>
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
-            <input type="text" ref="textInput" placeholder="Type to add task"/>
-          </form>
+          <AccountsUIWrapper />
+          { this.props.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks"
+              />
+            </form> : ''
+          }
         </header>
  
         <ul>
@@ -71,11 +82,13 @@ class App extends Component {
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
   incompleteCount: PropTypes.number.isRequired,
+  currentUser: PropTypes.object,
 };
  
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, {sort:{createdAt:-1}}).fetch(),
     incompleteCount: Tasks.find({checked: {$ne:true}}).count(),
+    currentUser: Meteor.user(),
   };
 }, App);
