0299e0ce07af22e5eddfb75452860eb15bb3e0d0
tebenia <tebe.deye@gmail.com>
2016-06-02 08:01:47 +0700

    Publish and Subscribe

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,35 +1,69 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
-
-export const Tasks =  new Mongo.Collection('tasks');
+ 
+export const Tasks = new Mongo.Collection('tasks');
+ 
+if(Meteor.isServer){
+	Meteor.publish('tasks', function tasksPublication() {
+		return Tasks.find({
+			$or: [
+		        { private: { $ne: true } },
+		        { owner: this.userId },
+		      ],
+		});
+	});
+}
 
 Meteor.methods({
-	'tasks.insert'(text){
-		check(text, String);
+  'tasks.insert'(text) {
+    check(text, String);
+ 
+    // Make sure the user is logged in before inserting a task
+    if (! this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
 
-		if(!this.userId){
-			throw new Meteor.Error("Not Authorized");
-		}
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
 
-		Tasks.insert({
-			text,
-			createdAt: new Date(),
-			owner: this.userId,
-			username: Meteor.users.findOne(this.userId).username,
-		});
-	},
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate){
+  	check(taskId, String);
+  	check(setToPrivate, Boolean);
 
-	'tesks.remove'(taskId) {
-		check(taskId, String);
+  	const task = Tasks.findOne(taskId);
 
-		Tasks.remove(taskId);
-	},
+  	if(task.owner !== this.userId){
+  		throw new Error('not-authorized');
+  	}
 
-	'tasks.setChecked'(taskId, setChecked) {
-	    check(taskId, String);
-	    check(setChecked, Boolean);
-	 
-	    Tasks.update(taskId, { $set: { checked: setChecked } });
-    },
+  	Tasks.update(taskId, {$set:{private: setToPrivate}});
+  }
 });
